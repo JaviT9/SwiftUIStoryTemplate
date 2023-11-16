a29a1fd533a171048fd45d516c7a52ed3c789697
Javier Torres <jtorres23@msu.idserve.net>
2023-11-16 18:46:18 -0500

    Final Changes

--- SwiftUIStoryTemplate/Chapter4/TestingNavigation.swift
+++ SwiftUIStoryTemplate/Chapter4/TestingNavigation.swift
@@ -47,13 +47,14 @@ struct TestingNavigationpt2: View {
     @State var selectedWeapon = "Sword "
     var body: some View {
         ZStack{
-            Color.indigo
+            Color.black
             //.ignoresSafeArea()
             VStack {
               
                 let Weapons = ["Sword 🗡️", "Shield 🛡️", "Bow 🏹", "Conch Shell 🐚" ]
               
                 Text("Pixel finally makes it to the Digital Sea his final step in his journey before he can go home. Pixel enters the Digital Sea in search for the The Heart of the sea, a powerful gem-like artifact that can heal and protect Zenith The ocean's spirit, Scala, a tempestuous and unpredictable entity, challenged Pixel to prove his worthinessand face a terrifying beast in battle. Before the battle begins, Scala offers Pixel a weapon to fight against the beast. ")
+                    .foregroundColor(.white)
                 Image("Code")
                     .resizable()
                     .scaledToFit()
@@ -68,6 +69,7 @@ struct TestingNavigationpt2: View {
                 }
                 
                 Text("Pixel chooses the \(selectedWeapon). Scala releases a giant electric eel. Amidst epic waves and fierce battles with the sea creature, Pixel and Pythos emerge victorious. demonstrating their courage and resolve. Touched by their determination, Scala revealed the Heart of the Sea. Pixel grabs the Heart of the Sea and swims back to shore Finally Pixel and Pythos has finished the lengthy quest and is ready to head back to Zenith a beautiful, vast digital world currently full of glitches and malstorms everyday.")
+                    .foregroundColor(.white)
                 
                 
               
@@ -79,10 +81,11 @@ struct TestingNavigationpt3: View {
     @State var selectedWeapon = "Sword"
     var body: some View {
         ZStack{
-            Color.blue
+            Color.black
             //.ignoresSafeArea()
             VStack {
              Text(" Pixel arrives back home Zenith and head to the center of the malstorms and discovers that an evil virus, Xeros, was the cause of all that befell Zenith. Pixel knew what he/she had pulled out the three ancient artifacts and as the third artifact was pulled out they began to glow. The bright Gold of the Microchip shinned as brightly as the sun, a refreshing green from the Knowledge of the Forest, a electrifying blue from the Heart of the sea. The three artifacts flew into the air and combined creating a sonic boom that blew through all of Zenith, rebooting the land of Zenith and cleansing it of the evil virus Xeros")
+                    .foregroundColor(.white)
                 Image("Gem")
                     .resizable()
                     .scaledToFit()
