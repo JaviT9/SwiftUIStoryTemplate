fc682e41d0a17c51b562a4b018d1bb0bf2713dcc
Justin <justin2525@me.com>
2023-11-16 09:22:38 -0500

    chapter two image

--- SwiftUIStoryTemplate/Chapter2View.swift
+++ SwiftUIStoryTemplate/Chapter2View.swift
@@ -3,6 +3,30 @@ import SwiftUI
 struct Chapter2View: View {
     var body: some View {
         Text("Chapter 2")
+    
+            ZStack{
+                VStack{
+                    
+                    Image("Code")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 250)
+                    Spacer(minLength: 10)
+                    
+                    //                    .padding(.bottom, 100)
+                    //                    .frame(100,,50,100)
+                    
+                    HStack{
+                       Text("test")
+                        (Image(systemName: "theatermasks.fill"))
+                        (Image(systemName: "house"))
+                    }
+
+         
+        }
+    }
+
+
     }
 }
 
