5596ea5ab127e7a9a568cd5342b52eca19342a53
Javier Torres <jtorres23@msu.idserve.net>
2023-11-16 23:57:46 -0500

    Final Story Code

---
+++ SwiftUIStoryTemplate/Saving Zenith.swift
@@ -0,0 +1,22 @@
+//
+//  Saving Zenith.swift
+//  SwiftUIStoryTemplate
+//
+//  Created by Javier A Torres Jr on 11/16/23.
+//
+
+import SwiftUI
+
+struct Saving_Zenith: View {
+    var body: some View {
+        ZStack{
+            Image("SavingZenith")
+                .resizable()
+//                .ignoresSafeArea()
+        }
+    }
+}
+
+#Preview {
+    Saving_Zenith()
+}
